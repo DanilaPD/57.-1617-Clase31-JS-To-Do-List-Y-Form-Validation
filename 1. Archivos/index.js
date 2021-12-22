bc7fa9e87ada13668f211a25b0e5455196fb35db
unknown <danila.perezdiaz@gmail.com>
2021-12-21 22:05:31 -0300

    Form Validation

--- 1. Archivos/index.js
+++ 1. Archivos/index.js
@@ -11,15 +11,35 @@ const password2 = document.getElementById("pass2");
 form.addEventListener("submit", (e) => {
   // El "preventDefault" sirve para evitar el comportamiento por default, que, en este caso, es justamente, el "submit".
   e.preventDefault();
-  console.log("Apreté submit");
+  //* console.log("Apreté submit");
   validate();
 });
 
+//!https://regexr.com/3e48o
+// La "i" al final es "insensitive": No distingue mayúsculas de minúsculas.
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i;
+
+//!https://stackoverflow.com/questions/14850553/javascript-regex-for-password-containing-at-least-8-characters-1-number-1-uppe
+const pass1Regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+
+//* Para que no haya "varita seleccionada":
+// "-1" se puede usar para el "fuera de index", o sea, no existe.
+// "selectedIndex" dice qué "index" va a mostrar.
+wand.selectedIndex = -1;
+
 // Vamos a llamar cada validación en una sola "función":
 const validate = () => {
-  console.log("validate");
+  //* console.log("validate");
   // Tenemos un problema: Los "espacios" los toma como caracteres. Para evitar eso, usamos una función llamada "trim" : Va a quitar los espacios del principio y del final.
   const user = username.value.trim();
+  const mail = email.value.trim();
+  const pass1 = password1.value.trim();
+  const pass2 = password2.value.trim();
+  const userWand = wand.value; //No necesito "trimearlo".
+
+  //? Por esta razón, capturamos el "value" y no el "input":
+  //* console.log("Mail:", typeof mail, mail);
+  //* console.log("Email:", typeof email, email);
 
   // Vamos a hacer las "comprobaciones":
   if (user === "") {
@@ -32,9 +52,69 @@ const validate = () => {
   } else {
     inputSuccess(username);
   }
+
+  if (mail === "") {
+    let errorMessage = "El email no puede estar vacío.";
+    inputError(email, errorMessage);
+    // Esto se lee como "Si 'emailRegex' al testear el 'mail' da 'false',...".
+    // https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
+    // El "!" significa "NO da true".
+    // Sería igual a "emailRegex.test(mail) === false", pero, en nuestro caso, estaríamos usando una versión más compacta.
+  } else if (!emailRegex.test(mail)) {
+    let errorMessage = "El mail no es válido.";
+    inputError(email, errorMessage);
+  } else {
+    inputSuccess(email);
+  }
+
+  if (userWand === "") {
+    let errorMessage = "Seleccione su varita.";
+    inputError(wand, errorMessage);
+  } else {
+    inputSuccess(wand);
+  }
+
+  if (pass1 === "") {
+    let errorMessage = "El password no puede estar vacío.";
+    inputError(password1, errorMessage);
+  } else if (!pass1Regex.test(pass1)) {
+    let errorMessage =
+      "El password no es válido. Debe tener, al menos, 8 caracteres, una mayúscula, una minúscula y un número.";
+    inputError(password1, errorMessage);
+  } else {
+    inputSuccess(password1);
+  }
+
+  if (pass2 === "") {
+    let errorMessage = "El password no puede estar vacío.";
+    inputError(password2, errorMessage);
+  } else if (pass2 !== pass1) {
+    let errorMessage = "Los password no coinciden";
+    inputError(password2, errorMessage);
+  } else {
+    inputSuccess(password2);
+  }
+
+  //!
+  //? Pseudocódigo para poner un "ojito" en el "input" del "password"; poner el ícono con "absolute" desde CSS:
+  //* ojito.addEventListener("click", () => {
+  //* password1.setAttribute("type", "text");
+  //* ojito.classList.add("Ojo tachado");
+  //* })
+
+  //? Pseudocódigo para agregar "varita personalizada" al elegir "otra":
+  // Hay que tener un "input" que esté oculto. Al seleccionar "index 4" (que es el "otra"), remove(clase que oculte ese "index"). Entonces, si no tiene (!=) esa clase, que se compruebe que no está vacío.
+  // Revisar los "tabIndex". Calculo que, estando oculto el tab, no va a entrar a él, pero estén atentos.
+
+  //? Validar "términos y condiciones": Habría que chequear que el "checkbox" esté en "checked" (Si el "checkbox" está "chequeado", todo bien, que el botón de continuar y apretar se habilite, sino, no te va a permitir avanzar). Lo ideal sería que el botón de "submit" tenga un "setAttribute("disabled", "disabled")" si no se chequeó.
+  // Probablemente, esta función la pondría fuera del "validate" porque sino, ¿cómo chequeamos el botón de "submit"? Yo le pondría un "listener" al "check" y tendría todo en una función aparte.
+
+  //? Se puede hacer un "¿Estás seguro de que tus datos son correctos? Para hacerlo, acceder a la siguiente página: https://sweetalert2.github.io/"
+
+  //? Se pueden activar las etiquetas "i" cuando es "success" o "error".
 };
 
-// Vamos a validar si el "form" se rellenó bien:
+// Vamos a "validar" si el "form" se rellenó bien:
 const inputSuccess = (input) => {
   const inputParent = input.parentElement;
   // Con "querySelector", nos va a agarrar el primero en que la etiqueta coincida. Si fuera "querySelectorAll", nos agarraría todos.
@@ -42,7 +122,7 @@ const inputSuccess = (input) => {
   inputParent.classList.add("success");
   inputParent.classList.remove("error");
   small.innerHTML = "";
-  console.log("Success");
+  //* console.log("Success");
 };
 
 // O si se rellenó mal:
@@ -54,5 +134,5 @@ const inputError = (input, message) => {
   inputParent.classList.remove("success");
   small.innerHTML = message;
   small.classList.add("error");
-  console.log("Error");
+  //* console.log("Error");
 };
